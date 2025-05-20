cbc3990c3f9181a88505bdcb55905295c93249f5
luizlenharo <lflenharo@gmail.com>
2025-05-19 21:42:29 -0300

    implementa a query4

---
+++ querys/query4.sql
@@ -0,0 +1,31 @@
+-- Porcentagem de emissao dos top 5 produtos mais emissores na agropecuária
+
+WITH EM_PRODUTO AS (
+	-- Emissão por produto agropecuário em determinado ano e regiao
+	SELECT
+		NOME_PRODUTO AS PRODUTO,
+		SUM (QTD_EM) AS QTD_EM
+	FROM EMISSAO e 
+		JOIN PRODUTO p 
+			ON e.EM_COD_PRODUTO = p.COD_PRODUTO
+		JOIN LOCALIZACAO l
+			ON e.EM_COD_LOC = l.COD_LOC
+		JOIN ESTADO uf
+			ON l.LOC_COD_UF = uf.COD_UF
+		JOIN REGIAO r
+			ON uf.UF_COD_REG = r.COD_REG
+	WHERE (e.ANO_EM = '2005-01-01') AND (ATVGERAL_PRODUTO = 'Pecuária' OR ATVGERAL_PRODUTO = 'Agricultura') AND (NOME_REG = 'CENTRO-OESTE')
+	GROUP BY (PRODUTO)
+), TOTAL AS (
+    -- Quantidade total de emissão dos produtos agropecuários baseado na tabela EM_PRODUTO
+	SELECT
+		SUM (QTD_EM) AS TOTAL_EM
+	FROM EM_PRODUTO
+)
+SELECT 
+	PRODUTO,
+	QTD_EM,
+	ROUND((QTD_EM * 100 / TOTAL.TOTAL_EM) :: numeric, 2)AS PORCENTAGEM
+FROM EM_PRODUTO, TOTAL
+ORDER BY PORCENTAGEM DESC
+LIMIT 5
